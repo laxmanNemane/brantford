d95df572b34d948a84d9ac9936a0ed6278f44a71
laxmanprometteur <lnemane7@gmail.com>
2022-08-16 12:13:58 +0530

    sidebar modification

--- src/Layout/Sidebar.js
+++ src/Layout/Sidebar.js
@@ -1,104 +1,47 @@
 import React, { useState } from "react";
 import "../Styles/Sidebar.css";
-import Collapse from "react-bootstrap/Collapse";
+import { TiChartBar } from "react-icons/ti";
+import { BsBuilding } from "react-icons/bs";
 import {
-  AiOutlineRocket,
-  AiOutlineHome,
-  AiOutlineDown,
-  AiOutlineUp,
+  AiOutlineUsergroupAdd,
+  AiOutlineInsurance,
+  AiOutlineCreditCard,
+  AiOutlineWechat,
+  AiOutlineSetting,
+  AiOutlineHome
 } from "react-icons/ai";
-import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
-  const [open, setOpen] = useState(false);
-  const [propertyOpen, setPropertyOpen] = useState(false);
   return (
     <div className="sidebar_main shadow ">
-      <h5 className="Logo_admin fw-bold my-3 ">BrantFord</h5>
+      <h5 className="Logo_admin fw-bold text-center"><AiOutlineHome /></h5>
       <div className=" Sidebar_otption_section">
-        <p
-          onClick={() => setOpen(!open)}
-          aria-controls="example-collapse-text"
-          aria-expanded={open}
-        >
-          <AiOutlineRocket className="fs-3" /> Dashbord{" "}
-          <span className="arrow_indication_collapse mb-2">
-            {open ? <AiOutlineUp /> : <AiOutlineDown />}
-          </span>
-        </p>
-        <Collapse in={open}>
-          <div className="collpse-item">
-            <ul className="collapse_item_list list-unstyled">
-              <li>
-                <NavLink to="/" className="text-dark fw-bold ">
-                  Home
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="/" className="text-dark fw-bold">
-                  Booked
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="/" className="text-dark fw-bold">
-                  Properties
-                </NavLink>
-              </li>
-            </ul>
-          </div>
-        </Collapse>
-        <p>
-          <AiOutlineHome className="fs-4 mb-2 ms-1" /> Properties
-        </p>
-        <p>
-          <AiOutlineHome className="fs-4 mb-2 ms-1" /> Properties
-        </p>
-        <p>
-          <AiOutlineHome className="fs-4 mb-2 ms-1" /> Properties
-        </p>
-      </div>
-      <h5 className="ms-4 fw-bold">Proprty Detail</h5>
-      <div className=" Sidebar_otption_section_2">
-        <p
-          onClick={() => setPropertyOpen(!propertyOpen)}
-          aria-controls="example-collapse-text"
-          aria-expanded={propertyOpen}
-        >
-          <AiOutlineRocket className="fs-3 " /> Dashbord
-          <span className="arrow_indication_collapse mb-2">
-            {propertyOpen ? <AiOutlineUp /> : <AiOutlineDown />}
-          </span>
-        </p>
-        <Collapse in={propertyOpen}>
-          <div className="collpse-item">
-            <ul className="collapse_item_list list-unstyled">
-              <li>
-                <NavLink to="/" className="text-dark fw-bold ">
-                  Home
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="/" className="text-dark fw-bold">
-                  Booked
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="/" className="text-dark fw-bold">
-                  Properties
-                </NavLink>
-              </li>
-            </ul>
-          </div>
-        </Collapse>
-        <p>
-          <AiOutlineHome className="fs-4 mb-2 ms-1" /> Properties
-        </p>
-        <p>
-          <AiOutlineHome className="fs-4 mb-2 ms-1" /> Properties
-        </p>
-        <p>
-          <AiOutlineHome className="fs-4 mb-2 ms-1" /> Properties
-        </p>
+        <ul className="list-unstyled">
+          <li className="my-3">
+            <TiChartBar className="Sidebar_icons" />
+            Dashbord
+          </li>
+          <li className="my-3">
+            <BsBuilding  className="Sidebar_icons"/> properties
+          </li>
+          <li className="my-3">
+            <AiOutlineUsergroupAdd  className="Sidebar_icons"/>
+            Tenants
+          </li>
+          <li className="my-3">
+            <AiOutlineInsurance  className="Sidebar_icons"/>
+            Insurance
+          </li>
+          <li className="my-3">
+            <AiOutlineCreditCard  className="Sidebar_icons"/>
+            Income
+          </li>
+          <li className="my-3">
+            <AiOutlineWechat  className="Sidebar_icons" />
+            Chat
+          </li>
+        </ul>
+        <p className="sidebar-setting"><AiOutlineSetting className="setting_icon" /></p>
       </div>
     </div>
   );
