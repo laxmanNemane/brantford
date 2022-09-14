5ef9476d7adf0ab3f544a7a818bbce7d31d934e0
laxmanprometteur <lnemane7@gmail.com>
2022-09-14 18:00:57 +0530

    set toast on category  in admin

--- src/EndUserPanel/OfficesDetailPage.js
+++ src/EndUserPanel/OfficesDetailPage.js
@@ -19,6 +19,7 @@ import { GrAttachment } from "react-icons/gr";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import { number } from "echarts";
+import HocLandingPage from "../Components/HocLandingPage";
 // import swal from 'sweetalert';
 
 const BaseUrl = "http://bantford.prometteur.in";
@@ -684,4 +685,4 @@ const OfficesDetailPage = () => {
   );
 };
 
-export default OfficesDetailPage;
+export default HocLandingPage(OfficesDetailPage);
