812064628c4193207cf8a82a48e2a0d896fe415e
laxmanprometteur <lnemane7@gmail.com>
2022-09-23 10:56:57 +0530

    navbar logo fixed acording screeen size

--- src/Layout/Navbar.js
+++ src/Layout/Navbar.js
@@ -6,7 +6,10 @@ import logo from "../Assets/Icons/logo.png";
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light ">
-      <div className="container py-2">
+      <div className="container ">
+        <NavLink className="navbar-brand d-lg-none" to="/">
+          <img src={logo} alt="" className="rounded-2 mx-3" />
+        </NavLink>
         <button
           className="navbar-toggler"
           type="button"
@@ -19,16 +22,14 @@ const Navbar = () => {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav mb-1 ms-auto">
+          <ul className="navbar-nav mb-1 mx-auto align-items-center">
             <li className="nav-item">Co-Working</li>
             <li className="nav-item">Commercial</li>
-          </ul>
-        </div>
-        <NavLink className="navbar-brand " to="/">
-          <img src={logo} alt="" className="rounded-2 mx-3" />
-        </NavLink>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav mb-1">
+
+            <NavLink className="navbar-brand d-none d-lg-block" to="/">
+              <img src={logo} alt="" className="rounded-2 mx-3" />
+            </NavLink>
+
             <li className="nav-item">
               <Link to="/allrentedspaces">For Rent</Link>{" "}
             </li>
